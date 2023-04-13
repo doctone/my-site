0a9304fbce662547f6d3bc12b01b56293a3b1858
Sam James <samjojames@gmail.com>
2023-04-13 22:00:54 +0100

    Add tests for themechanger

--- src/app/components/ThemeChanger/ThemeChanger.tsx
+++ src/app/components/ThemeChanger/ThemeChanger.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useTheme } from "next-themes";
-import { SunIcon } from "@chakra-ui/icons";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 
 export const ThemeChanger = () => {
   const { theme, setTheme } = useTheme();
@@ -11,7 +11,7 @@ export const ThemeChanger = () => {
         onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
         className="bg-none transition-all duration-100 px-8 py-2 text-2xl md:text-4xl hover:text-1xl rounded-lg absolute bottom-12 left-12"
       >
-        <SunIcon />
+        {theme === "dark" ? <SunIcon /> : <MoonIcon />}
       </button>
     </div>
   );
